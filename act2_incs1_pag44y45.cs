627c48e61bc82a1976885e8844389e132564c892
26FGD01-FatimaGonzalez <gonzalezfatima16db@gmail.com>
2026-05-01 16:25:58 -0600

    Add files via upload

---
+++ act2_incs1_pag44y45.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double  monedas=0;
+        string cantidad;
+        while (monedas<350)
+        {
+            Console.WriteLine("Ingrese la cantidad de monedas que obtuvo: ");
+            cantidad = Console.ReadLine();
+            double puntos = double.Parse(cantidad);
+            monedas = monedas+puntos;
+            if (monedas < 350)
+            Console.WriteLine("No tienes las monedas suficientes para desbloquear el nivel");
+            Console.WriteLine("Te hacen falta: "+ (monedas-350) + " monedas");
+            
+        }
+        Console.WriteLine("¡¡Haz llegado al nivel 5!!");
+    }
+}
